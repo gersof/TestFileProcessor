62453dd1e634e32c9ca49020a42d06af08e70853
Esteban Angulo <jangulorama@gmail.com>
2018-06-22 17:28:47 -0500

    Agregar archivos de proyecto.

---
+++ TestFileProcessor/FileProcessorService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TestFileProcessor
+{
+    public partial class FileProcessorService : ServiceBase
+    {
+        private System.Timers.Timer timerCounter=null;
+        private int counter = 0; 
+
+        public FileProcessorService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timerCounter = new System.Timers.Timer();
+            timerCounter.Interval = 10000;
+            timerCounter.Elapsed += new System.Timers.ElapsedEventHandler(timerCounter_Elapset);
+            timerCounter.Enabled = true;
+            timerCounter.Start();
+        }
+
+        void timerCounter_Elapset(object sender, ElapsedEventArgs e)
+        {
+            ExecuteProccess();
+        }
+
+        private void ExecuteProccess()
+        {
+            FilesProcessor fileProcessor = new FilesProcessor();
+            fileProcessor.ProcessFiles();
+
+            timerCounter.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ TestFileProcessor/FilesProcessor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFileProcessor
+{
+    public class FilesProcessor
+    {
+        const string inputDirectory= @"C:\TestFileProcessor\input";
+        const string outputDirectory = @"C:\TestFileProcessor\output\";
+
+        public void ProcessFiles()
+        {
+            this.GetFiles();
+        }
+
+        private void GetFiles()
+        {
+            DirectoryInfo d = new DirectoryInfo(inputDirectory);//Assuming Test is your Folder
+            FileInfo[] Files = d.GetFiles("*.txt"); //Getting Text files
+            string str = "";
+            foreach (FileInfo file in Files)
+            {
+                file.CopyTo(outputDirectory+file.Name);
+                file.Delete();
+            }
+
+        }
+    }
+}
